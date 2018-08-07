b949bfe74ab7fafc28b6051f4d435d33e7cd4a5d
Murilodsv <murilodsv@usp.br>
2018-08-07 15:56:45 -0300

    deleted

--- temp.sol
+++
@@ -1,9 +0,0 @@
-*Soils: Murilo Vianna (MV) Soil Template for DSSAT
-
-*<soil_id>  TEMP        C       <max_dp> TEMP DATABASE
-@SITE        COUNTRY          LAT     LONG SCS FAMILY
- <site> <country> <lat>  <lon> <soil_class>
-@ SCOM  SALB  SLU1  SLDR  SLRO  SLNF  SLPF  SMHB  SMPX  SMKE
-    BN <salb> <slu1> <sldr> <slro> <slnf> <slpf> SA001 SA001 SA001
-@  SLB  SLMH  SLLL  SDUL  SSAT  SRGF  SSKS  SBDM  SLOC  SLCL  SLSI  SLCF  SLNI  SLHW  SLHB  SCEC  SADC
- <slb> <slmh> <slll> <sdul> <ssat> <srgf> <ssks> <sbdm> <sloc> <slcl> <slsi> <slcf> <slni> <slhw> <slhb> <scec> <sadc>
